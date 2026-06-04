b520afb6eeb925bf72c55529e63e5326d412fa57
Luka Brdar <luka.brdar@tuta.io>
2026-06-04 22:31:11 +0200

    Fixed error.

--- app/src/main/java/com/lukabrdar/homecontrol/MainFragment.java
+++ app/src/main/java/com/lukabrdar/homecontrol/MainFragment.java
@@ -1,15 +1,16 @@
 package com.lukabrdar.homecontrol;
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -61,10 +62,14 @@ public class MainFragment extends Fragment {
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_main, container, false);
+    }
 
-        View view = inflater.inflate(R.layout.fragment_main, container, false);
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
         navController = Navigation.findNavController(view);
 
         btnContinue = view.findViewById(R.id.btn_continue);
@@ -72,7 +77,5 @@ public class MainFragment extends Fragment {
         btnContinue.setOnClickListener(v -> {
             navController.navigate(R.id.devicesFragment);
         });
-
-        return view;
     }
 }
